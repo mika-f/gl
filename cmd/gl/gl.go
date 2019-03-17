4583e9fd4ec231bcb21558b27a4990212a84fe0b
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-03-17 22:58:39 +0900

    feat: add subcommand

--- cmd/gl/gl.go
+++ cmd/gl/gl.go
@@ -3,14 +3,56 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli"
 )
 
+func generate(license string, name string, year int, output string) {
+	println(license)
+	println(name)
+	println(year)
+	println(output)
+}
+
 func main() {
+	var author string
+	var year int
+	var output string
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:        "author",
+			Usage:       "author name",
+			Destination: &author,
+		},
+		cli.IntFlag{
+			Name:        "year",
+			Usage:       "copyright year",
+			Destination: &year,
+		},
+		cli.StringFlag{
+			Name:        "output",
+			Usage:       "output path",
+			Destination: &output,
+		},
+	}
+
 	app := cli.NewApp()
 	app.Name = "gl"
-	app.Usage = "Generate a LICENSE file from command line"
+	app.Usage = "Generate a LICENSE file for your project"
+	app.UsageText = "gl <license> [--author author] [--year year]"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:  "mit",
+			Usage: "Generate LICENSE file as MIT",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				generate("mit", author, year, output)
+				return nil
+			},
+		},
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
